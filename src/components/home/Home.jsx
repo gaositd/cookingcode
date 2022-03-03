964417e6705396053f05b78eb5e7938d917d22ab
Alvaro Gamez Chavez <gaositd@gmail.com>
2022-03-03 13:01:52 -0600

    Changes to Home.jsx and home.css

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
- 
+ import './home.css';
+
 export function Home(){
   return(
-    <h1>Soy Home</h1>
+    <div>
+      <p className="presentation">
+      We are a small group of people with a common interest, the development of software for the web, that is, WEB WITH STYLE and A PISCA OF LOVE.
+      </p>
+    </div>
   )
 }
